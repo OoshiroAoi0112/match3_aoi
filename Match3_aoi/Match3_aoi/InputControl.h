0739745732141f4a0d7159704c8989daa949667d
GG葵 <138738983+OoshiroAoi0112@users.noreply.github.com>
2023-07-14 12:20:35 +0900

    書き込み
    GameOverScene.h
    その他4つのファイルを追加しました

---
+++ Match3_aoi/Match3_aoi/InputControl.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/***********************
+*マクロ定義
+************************/
+
+
+/***********************
+*型定義
+************************/
+
+
+/***********************
+*プロトタイプ宣言
+************************/
+
+void Input_Initialize(void);
+void Input_Update(void);
+int Input_Escape(void);
+
+
+int GetOldKey(int Key);
+int GetNowKey(int key);
+int GetKeyFlg(int key);
+int GetMousePositionX(void);
+int GetMousePositionY(void);
